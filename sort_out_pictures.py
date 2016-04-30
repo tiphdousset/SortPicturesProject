e345b46399a80899fe45dc9dcd4485cfb08ef423
Tiphanie Dousset <tiphdousset@gmail.com>
2016-04-30 19:43:21 +0200

    Implemented recursivity - now works for directories and subdirectories

--- sort_out_pictures.py
+++ sort_out_pictures.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 from os import listdir, mkdir, rmdir
 from os.path import isfile, join, isdir,getmtime, getctime
 import time
@@ -9,36 +10,39 @@ from shutil import copy
 path = sys.argv[1]
 print ("path = "+path)
 list_of_files_and_folders = listdir(path)
-list_of_files = []
-temp_path = join(path,"temp")
+#temp_path = join(path,"temp")
+temp_path = join("/home/tiphanie", "temp")
+errors = []
 
 mkdir(temp_path)
 print("temp_path = "+temp_path)
 
-for elem in list_of_files_and_folders:
-    file = join(path,elem)
-    if isfile(file):
-        metadata = pyexiv2.ImageMetadata(file) 
-        metadata.read()
-        metadata.exif_keys
-        tag = metadata['Exif.Image.DateTime']
-        mydate = tag.raw_value
-        mydate = tag.value.strftime('%A'+'_'+'%d'+'_'+'%B'+'_'+'%Y')
-        print("my date = "+mydate)
-        new_path=join(temp_path,mydate)
-        print("new path = "+new_path)
-        if isdir(new_path):
-            print ("there is already one, we just add the picture")
-            pass
-        else:
-            print("folder not yet exists, let's create it")
-            mkdir(new_path)
-        copy(file,new_path)
+def move_file(file=''):
+        print("file to handle = "+file)
+        try:
+            metadata = pyexiv2.ImageMetadata(file) 
+            metadata.read()
+            #print(metadata.exif_keys)
+            tag = metadata['Exif.Image.DateTime']
+            mydate = tag.raw_value
+            mydate = tag.value.strftime('%d'+'_'+'%B'+'_'+'%Y')
+            #print("my date = "+mydate)
+            new_path=join(temp_path,mydate)
+            #print("new path = "+new_path)
+            if isdir(new_path):
+                #print ("there is already one, we just add the picture")
+                pass
+            else:
+                #print("folder not yet exists, let's create it")
+                mkdir(new_path)
+            copy(file,new_path)
+        except:
+            errors.append(file)
 
-        #print ("date = "+mydate[0:10])
-        #print ("date = "+tag.value.strftime('%A %d %B %Y, %H:%M:%S'))
-        #print ("date = "+tag.value.strftime('%A'+'_'+'%d'+'_'+'%B'+'_'+'%Y'))
-        
+for root, directories, filenames in os.walk(path):
+    for filename in filenames:
+        move_file(join(root,filename))
 
-
-
+if len(errors) > 0:
+    print('Files with error: ')
+    print(errors)
